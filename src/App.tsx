6a99071a9f66ea449dc78b13840d514328292a42
coderaGab <inthedoomed@gmail.com>
2026-03-24 10:54:02 -0300

    link

--- src/App.tsx
+++ src/App.tsx
@@ -876,7 +876,7 @@ const Contact = () => {
                   LinkedIn
                 </p>
                 <a
-                  href="https://linkedin.com/company/safe-mining-servicos-estratégicos"
+                  href="https://linkedin.com/company/safe-mining-serviços-estratégicos"
                   target="_blank"
                   rel="noreferrer"
                   className="text-brand-accent font-medium hover:underline"
